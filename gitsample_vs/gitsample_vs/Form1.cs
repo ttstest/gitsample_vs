af84fef9945708a94c0c4f61b92023ddf9f05969
Hironori Sakakibara <testttstaro@gmail.com>
2019-03-04 18:15:52 +0900

    Hello world追加

--- gitsample_vs/gitsample_vs/Form1.cs
+++ gitsample_vs/gitsample_vs/Form1.cs
@@ -16,5 +16,10 @@ namespace gitsample_vs
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
